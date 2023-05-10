fc466f07b6e4feb499ea664c48a300ffcf5fe311
Garien Elio <garienelio@Gariens-MacBook-Pro.local>
2023-05-10 05:41:44 -0400

    Finished the scraper

---
+++ scraper/runscraper.py
@@ -0,0 +1,41 @@
+from scraper.spiders.apexcycle import ApexCycleSpider
+from scraper.spiders.peakpower import PeakpowerSpider
+from scraper.spiders.peakpowerbarrie import PeakpowerbarrieSpider
+from scraper.spiders.powersports import PowersportsSpider
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+import sqlite3
+import os
+
+# Clearing the database
+if not os.path.exists('database'):
+    os.makedirs('database')
+
+conn = sqlite3.connect("database/database.db")
+cur = conn.cursor()
+cur.execute("""DROP TABLE IF EXISTS NewData""")
+cur.execute("""CREATE TABLE NewData
+                (ID         INTEGER  PRIMARY KEY  AUTOINCREMENT  NOT NULL,
+                URL        TEXT,
+                TITLE      TEXT,
+                YEAR       INTEGER,
+                MAKE       TEXT,
+                MODEL      TEXT,
+                COLOR      TEXT,
+                PRICE      REAL,
+                CONDITION  TEXT,
+                ODOMETER   REAL,
+                VIN        TEXT,
+                DEALER     TEXT,
+                STOCK_NUM  TEXT,
+                LOCATION   TEXT)""")
+
+conn.close()
+
+settings = get_project_settings()
+process = CrawlerProcess(settings)
+process.crawl(ApexCycleSpider)
+process.crawl(PeakpowerSpider)
+process.crawl(PeakpowerbarrieSpider)
+process.crawl(PowersportsSpider)
+process.start()
--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -7,35 +7,19 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import sqlite3
-
-
-# class ScraperPipeline:
-#     def process_item(self, item, spider):
-
-#         return item
-
+import os
 
 class SQLitePipeline:
     def __init__(self):
-        self.conn = sqlite3.connect("../../database/newdata.db")
+        self.conn = sqlite3.connect("database/database.db")
         self.cur = self.conn.cursor()
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS Motorcycle
-                            (ID         INTEGER  PRIMARY KEY  AUTOINCREMENT  NOT NULL,
-                             URL        TEXT,
-                             TITLE      TEXT,
-                             YEAR       INTEGER,
-                             MAKE       TEXT,
-                             MODEL      TEXT,
-                             COLOR      TEXT,
-                             PRICE      REAL,
-                             CONDITION  TEXT,
-                             ODOMETER   REAL,
-                             VIN        TEXT,
-                             DEALER     TEXT,
-                             STOCK_NUM  TEXT,
-                             LOCATION   TEXT)""")
 
 
     def process_item(self, item, spider):
+        self.cur.execute("""INSERT INTO NewData VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                         (item.get('url'), item.get('unitTitle'), item.get('year'), item.get('make'), item.get('model'), 
+                          item.get('color'), item.get('price'), item.get('condition'), item.get('odometer'), item.get('vin'), 
+                          item.get('dealer'), item.get('stockNum'), item.get('location')))
+        self.conn.commit()
 
         return item
